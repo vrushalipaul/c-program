edd2f231191189f92c02d1de987ac5de54c0663a
vrushalipaul <53295282+vrushalipaul@users.noreply.github.com>
2021-04-10 20:46:09 +0530

    Add files via upload

---
+++ addition program.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num1, num2;
+    num1=20;
+    num2=10;
+    int ans;
+    printf("\nvalue of num1=%d",num1);
+    printf("\nvalue of num2=%d",num2);
+    ans=num1+num2;
+    printf("\n value of ans is =%d",ans);
+    return 0;
+}
